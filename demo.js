403e7c4789a445655d14fe70a91aafb2a9450eb2
Kyle Robinson Young <kyle@dontkry.com>
2013-04-11 21:39:37 -0700

    3 grass to 1 grass array

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ var binaryXHR = require('binary-xhr')
 var fly = require('voxel-fly')
 
 var textures = "http://commondatastorage.googleapis.com/voxeltextures/"
-var materials = [["grass","grass","grass"],"lava2", "stone", "dirt", "coal", "gravel", "iron", "sandstone", "brick", "glass", "torch", "wool", "glasspane", "log", "woodstairs", "wood", "fence", "woodendoor", "cobble", "water2", "leaves", "mossy", "spawner", "diamond"]
+var materials = [["grass"], "lava2", "stone", "dirt", "coal", "gravel", "iron", "sandstone", "brick", "glass", "torch", "wool", "glasspane", "log", "woodstairs", "wood", "fence", "woodendoor", "cobble", "water2", "leaves", "mossy", "spawner", "diamond"]
 var pos = [122, 72, 286]
 var regionX = Math.floor((pos[0] >> 4) / 32)
 var regionZ = Math.floor((pos[2] >> 4) / 32)
